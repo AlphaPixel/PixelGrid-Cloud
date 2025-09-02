e50c1d05efcc0f1a844224b19746dbd066fb526e
Chris 'Xenon' Hanson <xenon@alphapixel.com>
2025-09-02 00:17:11 -0600

    Credits/links, color/title, ED default, favicon, logo, UI cue highlights, DL filename, upload preview. (#2)
    * Hover Tooltips, Disabling of scale/grid overlay, help popup
    
    * Add license and update README
    
    * Add credits/links at the bottom.
    
    * Fix typo with unintended backtick
    
    * README URLs, color changes, site title, make Error Diffusion the default
    
    * Add favicon
    
    * Adds background logo in primary display area until image is loaded.
    
    * Add UI cueing highlights to direct people to operate the correct buttons in order.
    
    * Preserve an modify uploaded image filename when downloading modified image.
    
    * Add original image preview after loading, prior to processing.
    
    * Add version number.

---
+++ version.js
@@ -0,0 +1,25 @@
+const PixelGridReleaseMajor = 0;
+const PixelGridReleaseMinor = 1;
+const PixelGridReleaseSub   = 0;
+const PixelGridReleaseName  = "Goya";
+
+// Construct immutable string
+const PixelGridReleaseString =
+  `${PixelGridReleaseMajor}.${PixelGridReleaseMinor}.${PixelGridReleaseSub} ${PixelGridReleaseName}`;
+
+/* Release codename list
+
+Francisco Goya
+Caravaggio
+Peter Paul Rubens
+Claude Monet
+Frida Kahlo
+Albrecht Dürer
+Salvador Dalí
+Rembrandt
+Raphael
+Pablo Picasso
+Vincent van Gogh
+Michelangelo
+
+*/
